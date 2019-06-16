1a2afe67b680a0d11e2c26489c939f1fe8222492
Aws Al Rubaye <aws.kamil@gmail.com>
2019-06-16 12:34:29 -0500

    New Feed Selection Test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -96,11 +96,22 @@ $(function () {
     })
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function () {
 
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
+        beforeEach(function (done) {
+            $('.feed').empty();
+            loadFeed(0, function () {
+                feedEntry = $('.feed').find(allFeeds.url);
+                done();
+            });
+        });
 
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
-
+        it('content actually changes', function () {
+            expect(feedEntry).not.toBe(0);
+        });
+    });
 }());
